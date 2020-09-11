44d94ab2e05c33132ead0339a2a8244d9b59fd32
Behzad <bsadrfa@g.clemson.edu>
2020-09-11 02:09:32 -0400

    update trackers

--- oysterd/config.py
+++ oysterd/config.py
@@ -30,8 +30,9 @@ class Config:
         path = "/home/bsadrfa/behzad/projects/data_oyster/video/GH020870.MP4",
         fps = -1,
         fs = 2400,
-        fe = 2815,
-        tmp = "/scratch2/bsadrfa/oyster/tmp2/"
+        fe = 2412,
+#        tmp = "/scratch2/bsadrfa/oyster/tmp2/"
+        tmp = "/home/bsadrfa/behzad/projects/oyster//tmp2/"
 
     )
     SOLVER_IMS_PER_BATCH = 2
--- oysterd/stream.py
+++ oysterd/stream.py
@@ -2,7 +2,7 @@ import cv2
 import os
 from pathlib import Path
 from tqdm import tqdm
-from detectron2.utils.visualizer import Visualizer, GenericMask
+from detectron2.utils.visualizer import Visualizer, GenericMask, VisImage
 from detectron2.utils.visualizer import ColorMode
 from detectron2.structures import Boxes, BoxMode, pairwise_iou
 import torch
@@ -12,6 +12,7 @@ from os.path import isfile, join
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from copy import deepcopy
+from matplotlib import cm
 
 # initialize a dictionary that maps strings to their corresponding
 # OpenCV object tracker implementations
@@ -25,6 +26,10 @@ OPENCV_OBJECT_TRACKERS = {
     "mosse": cv2.TrackerMOSSE_create
 }        
 
+max_recs = 20
+rec_cmap = cm.get_cmap('tab20', max_recs)
+rec_colors = [[int(color[channel]*255) for channel in range(3)] for color in [rec_cmap(i) for i in range(max_recs)]]
+
 class Stream:
     def __init__(self, thing, vdata=None):
         self.thing = thing
@@ -36,8 +41,8 @@ class Stream:
         else:
             self.vdata = vdata
         self.tracker_type = "kcf"
-        self.trackers = []
-        self.tracker_color = [255, 0, 0]
+        self.trackers = dict()
+        self.tracked_boxes = dict()
         self.frame = None
         self.cur_frame = None
         self.prv_frame = None
@@ -46,68 +51,55 @@ class Stream:
         self.detected_frame = None
         self.updated_frame = None
         self.stacked_frame =  None
-        self.tracked_boxes = []
-        self.tracked_succes = []
         self.detected_boxes = None
         # input: tracked box
         # output: (mapped detected index, iou) 
-        self.box_pairs = dict() 
+        # self.box_pairs = dict() 
         self.bound_overlap_reomve = 0.25  # remove tracker for low overlaps
-        self.bound_overlap_high = 0.75  # re-init the tracker for high overlaps
+        self.bound_overlap_high = 0.5  # re-init the tracker for high overlaps
+        self.vis =  None
 
-    def detectFrame(self):
-        # oyster_metadata = self.thing.MetadataCatalog.get(self.thing.name + "_train")
-        # predictor = self.thing.predictor        
-        # Prediction
-        self.detected_frame = deepcopy(self.frame)
-
-        outputs = self.thing.predictor(self.detected_frame)
-        predictions = outputs["instances"].to("cpu")
-        # masks = (predictions.pred_masks.any(dim=0) > 0).numpy()
-        boxes = predictions.pred_boxes.tensor.numpy() if predictions.has("pred_boxes") else None
-        self.detected_boxes = boxes
-       
-        v = Visualizer(self.detected_frame[:, :, ::-1],
+    def initVisualizer(self, font_size=4):
+        self.vis = Visualizer(self.frame,
                     metadata=self.thing.metadata,
-                    scale=1,
                     instance_mode=ColorMode.SEGMENTATION
                     )
-        v._default_font_size *= 1.0
-        # v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-        v_boxes = predictions.pred_boxes if predictions.has("pred_boxes") else None
-        v = v.overlay_instances(
-            masks=None,
-            boxes=v_boxes,
-            labels=None,
-            keypoints=None,
-            assigned_colors=[(0,0.01,1) for i in range(len(v_boxes))],
-            alpha=0.8)
-        self.detected_frame = v.get_image()[:, :, ::-1]
-        # cv2.imwrite(os.path.join(in_dir, 'in_' + d["image_id"]),
-        #             v.get_image()[:, :, ::-1])      
-        # return detected_frame, boxes, masks
+        self.vis._default_font_size *= font_size
 
+    def resetVis(self):
+        self.vis.img = np.asarray(self.frame[:, :, ::-1]).clip(0, 255).astype(np.uint8)
+        self.vis.output = VisImage(self.vis.img)
+
+    def detectFrame(self):
+        self.detected_frame = deepcopy(self.frame)
+        outputs = self.thing.predictor(self.detected_frame)
+        predictions = outputs["instances"].to("cpu")
+        scores = predictions.scores if predictions.has("scores") else None
+        self.detected_boxes  = predictions.pred_boxes.tensor.numpy() if predictions.has("pred_boxes") else None
+        # masks = (predictions.pred_masks.any(dim=0) > 0).numpy()
+        # areas = np.prod(boxes[:, 2:] - boxes[:, :2], axis=1)
+        for i, box in enumerate(self.detected_boxes):
+            (x0, y0, x1, y1) = [int(v) for v in box]
+            cv2.rectangle(self.detected_frame, (x0, y0), (x1, y1), rec_colors[i%max_recs], 3)
+            cv2.putText(self.detected_frame, '{}'.format(i+1), (x0, y0+50), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 0, 100), 3, cv2.LINE_AA)
 
     def trackFrame(self):
         self.tracked_frame  = deepcopy(self.frame)
-
-        self.tracked_boxes = []
-        self.tracked_succes = []
-        for tracker in self.trackers:
-            (succes, box) = tracker.update(self.tracked_frame)
-            self.tracked_boxes.append(box)
-            self.tracked_succes.append(succes)
-
-        for box in self.tracked_boxes:
-            (x, y, w, h) = [int(v) for v in box]
-            cv2.rectangle(self.tracked_frame, (x, y), (x + w, y + h), self.tracker_color, 2)
-
-    def initTrackers(self):
-        for box in self.detected_boxes:
-            self.trackers.append(OPENCV_OBJECT_TRACKERS[self.tracker_type]())
-            self.trackers[-1].init(self.frame, 
-                (box[0], box[1], box[2]-box[0], box[3]-box[1])) # x, y, w, h
-            self.count += 1
+        removed_trackers = []
+        for i, tracker in self.trackers.items():
+            (success, box) = tracker["tracker"].update(self.tracked_frame)
+            if success:
+                tracker["success"] = 0
+                self.tracked_boxes[i] = box
+                self.draw_box(self.tracked_frame, i, box)    
+            else:
+                if self.remove_tracker(i):
+                    removed_trackers.append(i) 
+                else:
+                    self.draw_box(self.tracked_frame, i, box)    
+        for i in removed_trackers:
+            del self.trackers[i]
+            del self.tracked_boxes[i]
 
     def countVideo(self, vdata=None):
         if not vdata:
@@ -145,6 +137,7 @@ class Stream:
                 self.cur_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
                 if not is_init:
                     self.prv_frame = self.cur_frame
+                    self.initVisualizer()
                     is_init = True
 
                 # detect things
@@ -164,6 +157,8 @@ class Stream:
 
                 # store stacked image
                 cv2.imwrite(os.path.join(tmpPath, '{:04d}.jpg'.format(frame_number)), self.stacked_frame)
+
+                cv2.imshow('{:04d}'.format(frame_number), self.stacked_frame)
 
                 # store last frame
                 self.prv_frame = self.cur_frame
@@ -188,84 +183,109 @@ class Stream:
         video.release()
         pbar.close()
 
-
     def updateTrackers(self):
-        # track_boxmode = BoxMode.XYXY_ABS
-        tracked_boxes = torch.as_tensor(self.tracked_boxes).reshape(-1, 4)  # guard against no boxes
-        tracked_boxes = Boxes(tracked_boxes)
-        undetected_tracked_indcies = list(range(len(self.tracked_boxes)))
+        # track box mode: BoxMode.XYWH_ABS
+        tracked_boxes = Boxes(list(self.tracked_boxes.values()))
+        undetected_tracked_indcies = list(range(len(tracked_boxes)))
+        tracked_indices = list(self.tracked_boxes.keys())
 
-        detected_boxes = torch.as_tensor(self.detected_boxes).reshape(-1, 4)  # guard against no boxes
+        # detected box mode: BoxMode.XYXY_ABS
+        detected_boxes = BoxMode.convert(self.detected_boxes, BoxMode.XYXY_ABS, BoxMode.XYWH_ABS)
+        # detected box mode: BoxMode.XYWH_ABS
         detected_boxes = Boxes(detected_boxes)
-        new_detected_indcies = list(range(len(self.detected_boxes)))
+        new_detected_indcies = list(range(len(detected_boxes)))
 
-        overlaps = pairwise_iou(detected_boxes, tracked_boxes)
-        pairs = dict()
+        overlaps = pairwise_iou(tracked_boxes, detected_boxes)
+        # pairs = dict()
         # map the detected boxes to the tracked boxes
-        for _ in range(min(len(self.tracked_boxes), len(self.detected_boxes))):
-            # find which tracked box maximally covers each detected box
-            # and get the iou amount of coverage for each detected box
+        for _ in range(min(len(tracked_boxes), len(detected_boxes))):
+            # find which detected box maximally covers each tracked box
+            # and get the iou amount of coverage for each tracked box
             max_overlaps, argmax_overlaps = overlaps.max(dim=0)
+            # print(overlaps)
 
-            # find which tracked box is 'best' covered (i.e. 'best' = most iou)
-            tracked_ovr, tracked_ind = max_overlaps.max(dim=0)
-            assert tracked_ovr >= 0
-            # remove the tracked box from list
-            undetected_tracked_indcies.remove(tracked_ind)
-            
-            # find the detected box that covers the best tracked box
-            detected_ind = argmax_overlaps[tracked_ind]
+            # find which detected box is 'best' covered (i.e. 'best' = most iou)
+            ovr, detected_ind = max_overlaps.max(dim=0)
+            detected_ind = detected_ind.item()
+            ovr = ovr.item()
+            assert ovr >= 0
             # remove the detected box from list
             new_detected_indcies.remove(detected_ind)
+            
+            # find the tracked box that covers the best detected box
+            tracked_ind = argmax_overlaps[detected_ind]
+            # remove the tracked box from list
+            undetected_tracked_indcies.remove(tracked_ind)
+            # undetected_tracked_indcies.pop(tracked_ind)
             
             # record the iou coverage of and assigned detected box for this tracked box
             # input: tracked box
             # output: (mapped detected index, iou) 
-            pairs[tracked_ind] = (detected_ind, tracked_ovr)
+            # pairs[self.trackers_id[tracked_ind]] = (detected_ind, ovr)
 
             # if the tracker overlap is high enough, re-init the tracker 
-            if tracked_ovr>self.bound_overlap_high: 
+            if ovr>self.bound_overlap_high: 
                 # re-init the tracker with the detected box
-                box = self.detected_boxes[detected_ind]
-                self.trackers[tracked_ind].init(self.frame, 
-                    (box[0], box[1], box[2]-box[0], box[3]-box[1])) # x, y, w, h
-        
+                self.update_tracker(tracked_indices[tracked_ind], detected_boxes[detected_ind])
+
             # if the tracker overlap is not low, delete the tracker
-            if tracked_ovr<self.bound_overlap_reomve: 
-                new_detected_indcies.append(detected_ind)
-                undetected_tracked_indcies.append(tracked_ind)
+#            if ovr<self.bound_overlap_reomve: 
+#                new_detected_indcies.append(detected_ind)
+#                undetected_tracked_indcies.append(tracked_ind)
 
             # mark the tracked box and the detected box as used
-            overlaps[:, tracked_ind] = -1
-            overlaps[detected_ind, :] = -1
+            overlaps[tracked_ind, :] = -1
+            overlaps[:, detected_ind] = -1
 
         # add trackers for the new detected boxes
         for i in new_detected_indcies:
-            box = self.detected_boxes[i]
-            self.trackers.append(OPENCV_OBJECT_TRACKERS[self.tracker_type]())
-            self.trackers[-1].init(self.frame, 
-                (box[0], box[1], box[2]-box[0], box[3]-box[1])) # x, y, w, h
-            self.count += 1
+            self.add_tracker(detected_boxes[i])
 
         # remove track boxes that are not detected
         for i in sorted(undetected_tracked_indcies, reverse=True):
-            del self.trackers[i]
+            self.remove_tracker(tracked_indices[i])
 
         # draw the updated frame
         self.updated_frame = deepcopy(self.frame)
-        for box in self.tracked_boxes:
-            (x, y, w, h) = [int(v) for v in box]
-            cv2.rectangle(self.updated_frame, (x, y), (x + w, y + h), self.tracker_color, 2)
+        for i, box in self.tracked_boxes.items():
+            self.draw_box(self.updated_frame, i, box)    
 
-        self.box_pairs = pairs
+    def draw_box(self, frame, idx, box):
+        (x, y, w, h) = [int(v) for v in box]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), rec_colors[idx % max_recs], 3)
+        cv2.putText(frame, '{}'.format(idx), (x, y+50), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 0, 100), 3, cv2.LINE_AA)
+
+    def add_tracker(self, box):
+        self.count += 1
+        self.trackers[self.count]=dict(
+            tracker=OPENCV_OBJECT_TRACKERS[self.tracker_type](),
+            success=0  # new tracker
+            )
+        tuple_box = tuple(box[:].tensor.numpy()[0])
+        self.trackers[self.count]["tracker"].init(self.frame, tuple_box) # x, y, w, h
+        self.tracked_boxes[self.count]=tuple_box
+
+    def update_tracker(self, idx, box):
+        tuple_box = tuple(box[:].tensor.numpy()[0])
+        self.trackers[idx]["tracker"].init(self.frame, tuple_box) # x, y, w, h
+        self.trackers[idx]["success"] = 0 # updated tracker
+        self.tracked_boxes[idx]=tuple_box
+
+    def remove_tracker(self, idx, max_miss = 4):
+        self.trackers[idx]["success"] += 1
+        if self.trackers[idx]["success"] > max_miss:
+            return True
+        else:
+            return False
 
     def calcFlow(self):
-        flow = cv2.calcOpticalFlowFarneback(self.prv_frame, self.cur_frame, None, 0.5, 3, 7, 3, 5, 1.2, 0)
-        h, w = self.frame.shape[:2]
-        bulk_flow = flow.sum(axis=(0, 1))/(h*w)
-        center = (int(w/2), int(h/2))
-        self.flow_frame = cv2.arrowedLine(self.frame, center, 
-            (center[0]+int(bulk_flow[0]*500), center[1]+int(bulk_flow[1]*500)), (255, 0, 0), 5)
+        self.flow_frame = self.frame
+        # flow = cv2.calcOpticalFlowFarneback(self.prv_frame, self.cur_frame, None, 0.5, 3, 7, 3, 5, 1.2, 0)
+        # h, w = self.frame.shape[:2]
+        # bulk_flow = flow.sum(axis=(0, 1))/(h*w)
+        # center = (int(w/2), int(h/2))
+        # self.flow_frame = cv2.arrowedLine(self.frame, center, 
+        #     (center[0]+int(bulk_flow[0]*500), center[1]+int(bulk_flow[1]*500)), (255, 0, 0), 5)
 
         # my_dpi = 100
         # fig = plt.figure(figsize=(w/my_dpi, h/my_dpi), dpi=my_dpi)
@@ -297,7 +317,7 @@ class Stream:
             cv2.resize(self.tracked_frame, dim, interpolation = cv2.INTER_AREA),
             cv2.resize(self.updated_frame, dim, interpolation = cv2.INTER_AREA))
         # labels
-        labels = [np.ones(shape=[pad, int(dim[0]), 3], dtype=np.uint8) for i in range(len(frames))]
+        labels = [np.ones(shape=[pad, int(dim[0]), 3], dtype=np.uint8)*255 for i in range(len(frames))]
         # texts
         texts = (
             "Oysters: {}".format(self.count), "Detected", "Tracked", "Detected + Tracked"
@@ -306,7 +326,7 @@ class Stream:
         org = (int(0.7*pad), int(0.7*pad))
         labels = [cv2.putText(l, texts[i], org, cv2.FONT_HERSHEY_SIMPLEX, .7, (125, 255, 0), 1, cv2.LINE_AA) for i, l in enumerate(labels)]
         # vertical splitter pad
-        splitter = np.ones(shape=[int(dim[1]+pad)*2, pad, 3], dtype=np.uint8)
+        splitter = np.ones(shape=[int(dim[1]+pad)*2, pad, 3], dtype=np.uint8)*255
         # stack labels and frames
         frames = [np.concatenate((f, l), axis=0) for (f,l) in zip(frames, labels)]
         self.stacked_frame = np.concatenate((
